cdc8a0ada6e7cd00bac59b1ab97766213641a670
gaddopur <60126403+gaddopur@users.noreply.github.com>
2021-02-13 21:09:00 +0530

    Update settings.py

--- prastuti/settings.py
+++ prastuti/settings.py
@@ -23,6 +23,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+PREPEND_WWW = True
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
