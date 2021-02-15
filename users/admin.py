3b7f5e262d9b0435193d22dba6eb775af5a708ed
Himanshu Balasamanta <himanshubb.eee18@itbhu.ac.in>
2021-02-15 10:45:57 +0530

    exporting work done
    Signed-off-by: Himanshu Balasamanta <himanshubb.eee18@itbhu.ac.in>

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from import_export.admin import ImportExportModelAdmin
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import get_user_model
 # from .models import CustomUser
@@ -7,7 +8,7 @@ from django.contrib.auth import get_user_model
 CustomUser = get_user_model()
 
 @admin.register(CustomUser)
-class CustomUserAdmin(DjangoUserAdmin):
+class CustomUserAdmin(DjangoUserAdmin,ImportExportModelAdmin):
     """Define admin model for custom CustomUser model with no email field."""
 
     fieldsets = (
