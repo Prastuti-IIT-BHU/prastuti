8d233652c6dcb3c064d853864d245d2cbbba348e
Himanshu Balasamanta <himanshubb.eee18@itbhu.ac.in>
2021-02-16 08:50:25 +0530

    event ids added
    Signed-off-by: Himanshu Balasamanta <himanshubb.eee18@itbhu.ac.in>

--- teams/admin.py
+++ teams/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Team
-from import_export.admin import ImportExportModelAdmin
+# from import_export.admin import ImportExportModelAdmin
 
 
 @admin.register(Team)
-class TeamAdmin(ImportExportModelAdmin):
+class TeamAdmin(admin.ModelAdmin):
     list_display = ('team_name','team_event','team_active')
