6a5dda23b0dcd8644d60df80a5bdaca6c0609fb3
gaddopur <60126403+gaddopur@users.noreply.github.com>
2021-02-17 15:00:18 +0530

    Update settings.py

--- prastuti/settings.py
+++ prastuti/settings.py
@@ -29,7 +29,7 @@ PREPEND_WWW = True
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['prastuti-21.herokuapp.com', 'localhost', '127.0.0.1', '0.0.0.0', 'electricalfest.in']
 
